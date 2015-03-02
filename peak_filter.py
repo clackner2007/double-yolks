3b7991900b8894fede0afffc4cd4f97c77f10e93
Claire Lackner <clairelackner@gmail.com>
2015-03-02 11:06:21 +0900

    fixed up peak_filter to have reasonable inputs and arguments

--- peak_filter.py
+++ peak_filter.py
@@ -8,17 +8,14 @@ Created on Tue Oct 23 19:20:52 2012
 import argparse
 import sys,os,re
 import pyfits
-#import matplotlib.pyplot as plt
 import numpy as np
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
-#import matplotlib.cm
 import copy
 import scipy.ndimage
-import glob
-from cosm_distTable import CosmoDist
+from astropy import table
 import flood_fill as ff
 import fluxSinc
 from multiprocessing import Pool
+from functools import partial
 
 def sidePlot(ax1,img):
     mymap = copy.copy(matplotlib.cm.jet)
@@ -136,31 +133,21 @@ def getPeaks(image, thresh):
     return areas
     
     
-def mapMeasPeak(galobj):
+def mapMeasPeak(galobj, r_in_fwhm=1.28, ring_width=0.7, fwhm_all=None):
     """
     mapping function that processes images for multithreading
     returns a list of peaks
     f is the filename
     """
     f = galobj['file']
-    fwhm = 0.09/0.03
-    fileid = f.split('/')[-1].split('_')[-3]#[-8] #real
-    #fileid = f.split('/')[-1].split('_')[-1].split('.')[0] #fake mocks
-    raS, decS = os.path.splitext(os.path.basename(f))[0].split("_")[-2:]#[-7:-5]
-    #raS, decS = (fileid,fileid)
-    
-    if galobj['inkpc']:
-        #kpc in pixels
-        kpc_1 = 1.0/(galobj['z'])*206265.0/0.03
-        if kpc_1 > 100.:
-            kpc_1= 5*fwhm
+    if fwhm_all is None:
+        fwhm = galobj['fwhm']
+    else:
+        fwhm = fwhm_all
 
     img=pyfits.open(f)[0].data
-    #r_in = fwhm*1.5 if not args.inkpc else max(kpc_1,fwhm*1.3)
-    r_in = fwhm*1.3 if not galobj['inkpc'] else max(kpc_1,fwhm*1.3)
-    #r_in = fwhm*2.3 if not args.inkpc else max(kpc_1,fwhm*1.3)
-    r_in = fwhm/2.35*3.0
-    r_out = r_in + 0.7
+    r_in = r_in_fwhm *fwhm
+    r_out = r_in + ring_width
     nx=np.round((r_out + 1.5)*2)
     lowpass=scipy.ndimage.median_filter(img,
                                         footprint=
@@ -178,13 +165,13 @@ def mapMeasPeak(galobj):
     peak_list = peak_list[np.where(np.array([p.size>8
                             for p in peak_list]))[0]]
     
-    galine = '%d %.2f %.2e %s %s '%(galobj['id'], galobj['z'], galobj['delz'], raS, decS)
-    galine += '%.2f %s %s %s %d\n'%(galobj['mag'], fileid, raS, decS, len(peak_list))
+    galine = '%d %.2f %.2e '%(galobj['id'], galobj['z'], galobj['delz'])
+    galine += '%.2f %d\n'%(galobj['mag'], len(peak_list))
                             
 
     pstring = ''
     for ip, p in enumerate(peak_list):
-            pstring += '%d %s %s %d '%(galobj['id'], raS, decS, ip)
+            pstring += '%d %d '%(galobj['id'], ip)
             ms=p.getMoments()
             pstring += '%d %.2e '%(p.size, (ms[1]/ms[0]))
             pstring +='%.7f '%p.getCenter()[0]+'%.7f '%p.getCenter()[1]
@@ -202,7 +189,7 @@ def main():
     parser.add_argument('infile',  help='input file list')
     parser.add_argument('-p', '--path', help='path to output files',
                         default='./', dest='path')
-    parser.add_argument('-k', '--kpc', help='do rings in kpc',
+    parser.add_argument('-k', '--kpc', help='do rings in kpc, not supported',
                         default=False, action='store_true',
                         dest='inkpc')
     parser.add_argument('-i', '--impath', dest='impath',
@@ -211,29 +198,42 @@ def main():
     parser.add_argument('-d', '--deltaz', default=0.0, type=float,
                         dest='delz',
                         help='redshift offset from original image')
+    parser.add_argument('-j', '--threads', default=1, dest='nthreads', 
+                        help='number of threads to run', type=int)
+    parser.add_argument('-r', '--ringInner', dest='r_in',
+                        type=float, default=1.28,
+                        help='inner ring limit in units of the FWHM')
+    parser.add_argument('-w', '--width', dest='ring_width', type=float,
+                        default=0.7, help='ring filter width in PIXELS')
+    parser.add_argument('-f', '--fwhm', default=None, dest='fwhm', type=float,
+                        help="""FWHM of PSF in PIXELS for all input galaxies, if none(default)
+                        the FWHM needs to be in the input file""")
 
     args=parser.parse_args()
 
-    #get the files
-#    id, ra, dec, zphot = np.loadtxt(args.infile,
-#                                    unpack=True,
-#                                    dtype=[('a', int),
-#                                           ('b', float),
-#                                           ('c', float),
-#                                           ('e', float)],
-#                                    usecols=(0,1,2,3))
-    ident, zphot, mag, filename = np.loadtxt(args.infile,
-                                     unpack=True,
-                                     dtype=[('a', int),
-                                            ('b', float), ('c', float),
-                                            ('f', '|S80')],
-                                            usecols=(0,2,3,1))
+    #get the data
+    data = table.Table().read(args.infile, 
+                        format='fits' if os.path.splitext(args.infile)[1]=='.fits' else 'ascii')
+    ident = data['ID']
+    zphot = data['Z']
+    mag = data['MAG']
+    filename = data['FILENAME']
+    if args.fwhm is None:
+        try:
+            fwhm = data['FWHM']
+        except KeyError:
+            raise KeyError("Either include PSF FWHM in input file or specify on command line")
+    else:
+        fwhm = np.zeros(ident.shape)
+    
     try:
-        delz = np.loadtxt(args.infile, dtype=float, usecols=(4,))
-    except:
+        delz = data['DELZ']
+    except KeyError:
         delz = np.zeros_like(ident) + args.delz
-
-    impath = args.impath #'/data/cosmos_data/photodata/imgs/'
+   
+    impath = args.impath
+    
+    #if there is a delta z set, then look for those files
     if args.delz != 0:
         files = [impath+'z%0.2f_'%(zphot[i])+filename[i]
                 for i in range(len(ident))]
@@ -250,32 +250,28 @@ def main():
                     openOK = True
                 except IOError:
                     files[nf] = impath+'z%0.2f_'%(zphot[nf]-0.0001)+filename[nf]
-
     else:
         files = [impath+filename[i] for i in range(len(ident))]
 
-#    files = [impath+'%06d_%.5f_%.5f.fits'%(id[i], ra[i], dec[i])
- #            for i in range(len(id))]
-    if args.inkpc:
-        z_list = np.loadtxt('/data/cosmos_data/id_ra_dec_z',
-                            usecols=[1,2,3])
-        cc = CosmoDist()
-        ad = cc.ang_dist(z_list[:,2])*1000.0
-        z_dict = dict(zip(zip(z_list[:,0], z_list[:,1]), ad))
-
 
     gals = np.array(zip(ident, files, zphot, mag, 
-                        delz, np.repeat(args.inkpc, len(ident))), 
+                        delz, fwhm, np.repeat(args.inkpc, len(ident))), 
                         dtype=np.dtype([('id', int), ('file', '|S128'),
-                        ('z', float), ('mag', float), ('delz', float), ('inkpc', bool)]))
+                        ('z', float), ('mag', float), ('delz', float), ('fwhm', float), ('inkpc', bool)]))
 
         
     gal_file = open(args.path+"gal_list", 'w')
-    peak_file = open(args.path+'peak_list', 'w')    
+    peak_file = open(args.path+'peak_list', 'w')
+    
+    #header for output
+    gal_file.write("ID Z DELTA_Z MAG NPEAKS\n")
+    peak_file.write("ID PEAK_ID PEAK_NPIX PEAK_B-A PEAK_X0_PIX PEAK_Y0_PIX PEAK_FLUX_FILTERED PEAK_FLUX\n")
 
     #multi-thread
-    pool = Pool(processes=12)
-    results = pool.map(mapMeasPeak, gals[:], len(gals[:])/(12*2)+1)
+    pool = Pool(processes=args.nthreads)
+    results = pool.map(partial(mapMeasPeak, r_in_fwhm=args.r_in,
+                               ring_width=args.ring_width, fwhm_all=args.fwhm), 
+                               gals[:], len(gals[:])/(args.nthreads*2)+1)
     pool.close()
     pool.join()
     
@@ -283,13 +279,6 @@ def main():
         gal_file.write(r[0])
         peak_file.write(r[1])
     
-
-    #single thread    
-#    for numf, f in enumerate(files[:1000]):     
-#        galine, peaklines = mapMeasPeak(gals[numf])         
-#        print >>gal_file, galine
-#        print >>peak_file, peaklines
-
     peak_file.close()
     gal_file.close()
 
