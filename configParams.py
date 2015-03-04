c1c39be11b31fad9f7c9635f0d04d63b08b1fe58
Claire Lackner <clairelackner@gmail.com>
2015-03-04 16:59:58 +0900

    added config file for general parameters

---
+++ configParams.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##############################
+# PROGRAM NAME
+#
+#
+##############################
+"""
+Created on Wed Mar  4 16:52:21 2015
+
+@author: clackner
+
+this just lists the configuration parameters that are used globablly
+"""
+
+#cosmology
+H0=70
+Om0=0.3
+
+#image properties
+zeropt=25.959
+pixelscale=0.03
+
+#cutout sizes in pixels
+cutoutX = 268
+cutoutY = 268
